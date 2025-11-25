b6e53865ec6da4b1233235817cf3b18010b80fd4
tau-lavender <129185870+tau-lavender@users.noreply.github.com>
2025-11-25 20:48:14 +0300

    upd main.py

---
+++ src/constants.py
@@ -0,0 +1,5 @@
+import os
+from pathlib import Path
+
+
+PATH_TO_ROOT = Path(os.path.abspath(__file__)) / ".." / ".."
--- src/main.py
+++ src/main.py
@@ -1,9 +1,30 @@
 import typer
+import subprocess
+import os
+from pathlib import Path
+
+from src.constants import PATH_TO_ROOT
 
 
-def main():
-    pass
+app = typer.Typer()
 
+@app.command()
+def main(
+    link: str,
+    dir: Path = ".",
+    ):
+    print(link, dir)
+    try:
+        if not(dir.exists()):
+            os.mkdir(dir)
+        os.chdir(dir)
+        subprocess.run(f"git clone {link}")
+        os.chdir(PATH_TO_ROOT)
+    except OSError as e:
+        print(f"OSError: {e}")
 
 if __name__ == "__main__":
-    main()
+    try:
+        app()
+    except OSError as e:
+        print(f"OSError: {e}")
