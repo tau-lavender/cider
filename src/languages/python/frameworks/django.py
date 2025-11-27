92619ddf016c73a655b22ba656c5b288467753e9
tau-lavender <129185870+tau-lavender@users.noreply.github.com>
2025-11-27 11:48:33 +0300

    fix django

--- src/languages/python/frameworks/django.py
+++ src/languages/python/frameworks/django.py
@@ -14,14 +14,14 @@ class PytestFramework(Framework):
         self.django_found = False
 
     def analyze(self, file_path: Path, file_contents: str):
-        if not self.pytest_found:
+        if not self.django_found:
             if ("import django" in file_contents
                 or "from django" in file_contents):
-                self.pytest_found = True
+                self.django_found = True
 
     def build(self):
         singleton = Singleton()
-        if self.pytest_found:
+        if self.django_found:
             print("* Detected Django")
             run_job = Job(runner="sh", command="python manage.py runserver")
             run_job.tags.add('python')
